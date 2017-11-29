423eba0fa84a9f3bab337e522f506045098822f1
Mark Lopez <m@silvenga.com>
2017-11-28 22:06:54 -0600

    Fixed bug where lightense backdrops exist between page loads.

--- src/src/components/lightense-loader.ts
+++ src/src/components/lightense-loader.ts
@@ -2,6 +2,18 @@ import Lightense from "lightense-images";
 
 export class LightenseLoader {
     public attachHandlers(): void {
+
+        let oldBackdrops = document.querySelectorAll(".lightense-backdrop");
+        for (let element of oldBackdrops) {
+            element.remove();
+        }
+        let oldStyleTags = document.querySelectorAll("style");
+        for (let element of oldStyleTags) {
+            if (/.*\.lightense-backdrop.*/g.test(element.innerText)) {
+                element.remove();
+            }
+        }
+
         let images = document.querySelectorAll(".s-article img");
         for (let image of images) {
             image.setAttribute("data-background", "rgba(255, 255, 255, 0.6)")
