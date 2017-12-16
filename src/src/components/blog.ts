dcd7ddebbcd924cfa0e46d13d30378669d0cb805
Mark Lopez <m@silvenga.com>
2017-12-15 22:26:24 -0600

    Added styling and javascript support for code headers.

--- src/src/components/blog.ts
+++ src/src/components/blog.ts
@@ -1,3 +1,4 @@
+import { Clipboard } from './clipboard';
 import { AjaxLoader } from './ajax-loader';
 import { GistHelper } from './gists/gist-helper';
 import { LightenseLoader } from './lightense-loader';
@@ -8,17 +9,11 @@ type SiteLoaded = () => void;
 
 export class Blog {
 
-    private _gistHelper: GistHelper;
-    private _piwik: Piwik;
-    private _ajaxLoader: AjaxLoader;
-    private _lightenseLoader: LightenseLoader;
-
-    constructor() {
-        this._gistHelper = new GistHelper();
-        this._piwik = new Piwik();
-        this._ajaxLoader = new AjaxLoader();
-        this._lightenseLoader = new LightenseLoader();
-    }
+    private _gistHelper: GistHelper = new GistHelper();;
+    private _piwik: Piwik = new Piwik();
+    private _ajaxLoader: AjaxLoader = new AjaxLoader();
+    private _lightenseLoader: LightenseLoader = new LightenseLoader();
+    private _clipboard: Clipboard = new Clipboard();
 
     private _onSiteLoaded: SiteLoaded[] = [
         () => console.log("Site is ready."),
@@ -41,6 +36,7 @@ export class Blog {
         () => this._gistHelper.findAndLoadGists(),
         () => this._lightenseLoader.attachHandlers(),
         () => this._ajaxLoader.attachHandlers(),
+        () => this._clipboard.attachCopyEvents(),
         (url, title, position, responseTime) => this._piwik.trackPageLoad(url, title, responseTime)
     ];
 
---
+++ src/src/components/clipboard.ts
@@ -0,0 +1,49 @@
+export class Clipboard {
+
+    public attachCopyEvents() {
+        let copyButtons = document.querySelectorAll<HTMLButtonElement>(`button[data-copy-id]`);
+        for (let copyButton of copyButtons) {
+            let copyId = copyButton.getAttribute("data-copy-id");
+            copyButton.onclick = (ev: MouseEvent) => {
+                this.copyTarget(copyId);
+                copyButton.blur();
+            };
+        }
+    }
+
+    private copyTarget(copyId: string) {
+        let target = document.querySelector(`[data-copy-target="${copyId}"]`)
+        let content = target.textContent;
+        this.copyText(content);
+    }
+
+    private copyText(text: string): boolean {
+
+        // https://stackoverflow.com/a/30810322/2001966
+        let textArea = document.createElement("textarea");
+        textArea.style.position = 'fixed';
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        textArea.style.width = '2em';
+        textArea.style.height = '2em';
+        textArea.style.padding = "0";
+        textArea.style.border = 'none';
+        textArea.style.outline = 'none';
+        textArea.style.boxShadow = 'none';
+        textArea.style.background = 'transparent';
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+
+        let success = false;
+        try {
+            let result = document.execCommand('copy');
+            success = result;
+        } catch (e) {
+            console.warn('Failed to copy:', e);
+        }
+
+        document.body.removeChild(textArea);
+        return success;
+    }
+}
