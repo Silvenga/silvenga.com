53d88e9bd9d3d8508101a956e253b2a61eac778e
Mark Lopez <m@silvenga.com>
2018-01-01 23:30:02 -0600

    Updated image reservation to handle responsive flows.

--- recipe/MarkdigExtensions/ImagePlaceholders/ImagePlaceholdersExtension.cs
+++ recipe/MarkdigExtensions/ImagePlaceholders/ImagePlaceholdersExtension.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Markdig;
 using Markdig.Renderers;
 using Markdig.Renderers.Html;
+using Markdig.Renderers.Html.Inlines;
 using Markdig.Syntax;
 using Markdig.Syntax.Inlines;
 
@@ -15,57 +16,19 @@ namespace recipe.MarkdigExtensions.ImagePlaceholders
 
         public void Setup(MarkdownPipelineBuilder pipeline)
         {
-            pipeline.DocumentProcessed -= PipelineOnDocumentProcessed;
-            pipeline.DocumentProcessed += PipelineOnDocumentProcessed;
         }
 
         public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
         {
-        }
-
-        private static void PipelineOnDocumentProcessed(MarkdownDocument document)
-        {
-            foreach (var node in document.Descendants())
+            var htmlRenderer = renderer as HtmlRenderer;
+            if (htmlRenderer != null)
             {
-                if (node is Inline)
+                if (!htmlRenderer.ObjectRenderers.Contains<ImagePlaceholdersRenderer>())
                 {
-                    var link = node as LinkInline;
-                    if (link != null && link.IsImage)
-                    {
-                        var resolvedImages = ResolveFirstImage(link.Url).ToList();
-                        if (resolvedImages.Count == 1)
-                        {
-                            var image = resolvedImages.Single();
-                            link.GetAttributes().AddProperty("data-real-path", image.RealPath);
-                            link.GetAttributes().AddProperty("height", image.Height.ToString());
-                            link.GetAttributes().AddProperty("width", image.Width.ToString());
-                        }
-                    }
+                    htmlRenderer.ObjectRenderers
+                        .Replace<LinkInlineRenderer>(new ImagePlaceholdersRenderer(BaseContentPaths));
                 }
             }
-        }
-
-        public static IEnumerable<(string RealPath, int Height, int Width)> ResolveFirstImage(string relativePath)
-        {
-            var resolvedPaths = BaseContentPaths
-                .Select(x =>
-                    {
-                        if (relativePath.StartsWith("/"))
-                        {
-                            relativePath = relativePath.Substring(1);
-                        }
-                        return Path.Combine(x, relativePath);
-                    })
-                .Select(x => Path.GetFullPath(x))
-                .Where(x => File.Exists(x))
-                .Select(x =>
-                {
-                    using (var image = SixLabors.ImageSharp.Image.Load(x))
-                    {
-                        return (x, image.Height, image.Width);
-                    }
-                });
-            return resolvedPaths;
         }
     }
 }
---
+++ recipe/MarkdigExtensions/ImagePlaceholders/ImagePlaceholdersRenderer.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Markdig;
+using Markdig.Renderers;
+using Markdig.Renderers.Html;
+using Markdig.Renderers.Html.Inlines;
+using Markdig.Syntax.Inlines;
+
+namespace recipe.MarkdigExtensions.ImagePlaceholders
+{
+    public class ImagePlaceholdersRenderer : LinkInlineRenderer
+    {
+        private const int MaxWidth = 780;
+
+        private ICollection<string> _baseContentPaths;
+
+        public ImagePlaceholdersRenderer(ICollection<string> baseContentPaths)
+        {
+            _baseContentPaths = baseContentPaths;
+        }
+
+        protected override void Write(HtmlRenderer renderer, LinkInline link)
+        {
+            if (link.IsImage)
+            {
+                var resolvedImages = ResolveImages(link.Url).ToList();
+                if (resolvedImages.Count == 1)
+                {
+                    var image = resolvedImages.Single();
+
+
+                    if (image.Width > MaxWidth || true)
+                    {
+                        var ratioPercent = image.Height / (decimal)image.Width * 100;
+
+                        var outerContainer = new HtmlAttributes();
+                        outerContainer.AddClass("img-container-outer");
+                        outerContainer.AddProperty("data-real-path", image.RealPath);
+                        outerContainer.AddProperty("style", $"max-width: {System.Math.Min(image.Width, MaxWidth)}px;");
+
+                        renderer.Write("<div");
+                        renderer.WriteAttributes(outerContainer);
+                        renderer.Write(">");
+                        {
+                            var imgContainerAttributes = new HtmlAttributes();
+                            imgContainerAttributes.AddClass("img-container");
+                            imgContainerAttributes.AddProperty("style", $"padding-top: {ratioPercent}%");
+                            renderer.Write("<div");
+                            renderer.WriteAttributes(imgContainerAttributes);
+                            renderer.Write(">");
+                            {
+                                renderer.Write("<p class=\"img-container-inner\">");
+                                base.Write(renderer, link);
+                                renderer.Write("</p>");
+                            }
+                            renderer.Write("</div>");
+                        }
+                        renderer.Write("</div>");
+
+                        return;
+                    }
+                }
+            }
+
+            base.Write(renderer, link);
+        }
+
+        public IEnumerable<(string RealPath, int Height, int Width)> ResolveImages(string relativePath)
+        {
+            var resolvedPaths = _baseContentPaths
+                .Select(x =>
+                    {
+                        if (relativePath.StartsWith("/"))
+                        {
+                            relativePath = relativePath.Substring(1);
+                        }
+                        return Path.Combine(x, relativePath);
+                    })
+                .Select(x => Path.GetFullPath(x))
+                .Where(x => File.Exists(x))
+                .Select(x =>
+                {
+                    using (var image = SixLabors.ImageSharp.Image.Load(x))
+                    {
+                        return (x, image.Height, image.Width);
+                    }
+                });
+            return resolvedPaths;
+        }
+    }
+}
