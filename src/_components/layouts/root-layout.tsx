6b2aa621fb1f00bfcd03d2eec29a6fe777b9311f
Mark Lopez <m@silvenga.com>
2024-06-11 18:37:35 -0500

    Added source code link to footer.

--- src/_components/layouts/root-layout.tsx
+++ src/_components/layouts/root-layout.tsx
@@ -120,9 +120,12 @@ function Navbar(this: RenderContext, { site, renderContent }: Pick<TemplateConte
 function Footer(): JSX.Element {
     const year = new Date().getFullYear();
     return (
-        <footer className="py-9 flex justify-center mt-auto">
+        <footer className="py-9 flex flex-col justify-center items-center mt-auto">
             <div className="text-center">
-                Copyright © {year}. Built with <span className="text-[#ad4d4d] dark:text-[#deabab]">&#x2764;&#xfe0e;</span> by <a className="link link-hover" href="https://github.com/Silvenga" rel="noreferrer noopener" target="_blank">Silvenga</a>
+                Copyright © {year}. Built with <span className="text-[#ad4d4d] dark:text-[#deabab]">&#x2764;&#xfe0e;</span> by Silvenga
+            </div>
+            <div className="text-center">
+                <a className="link link-hover" href="https://github.com/Silvenga/silvenga.com" rel="noreferrer noopener" target="_blank">[Source Code]</a>
             </div>
         </footer>
     )
