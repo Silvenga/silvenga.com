e8c65edcd6259bd4e55d3b33fbb69d6f79dd67cf
Mark Lopez <m@silvenga.com>
2024-06-13 08:54:49 -0500

    Added rel=me to social links.

--- src/_components/about.tsx
+++ src/_components/about.tsx
@@ -10,10 +10,10 @@ export function About({ className }: { className?: string }) {
                 I love Software Engineering and work with everything from Ceph to Kubernetes. In my free-time I&apos;m learning CAD and enjoying 3D-printing.
             </p>
             <div className="flex lg:justify-start justify-center">
-                <a href="https://github.com/Silvenga" target="_blank" rel="noreferrer noopener" className="me-3">
+                <a href="https://github.com/Silvenga" target="_blank" rel="me noreferrer noopener" className="me-3">
                     <GithubIcon className="h-6 w-6 aspect-square" />
                 </a>
-                <a href="https://slvn.social/@silvenga" target="_blank" rel="noreferrer noopener" className="me-3">
+                <a href="https://slvn.social/@silvenga" target="_blank" rel="me noreferrer noopener" className="me-3">
                     <MastodonIcon className="h-6 w-6 aspect-square" />
                 </a>
             </div>
