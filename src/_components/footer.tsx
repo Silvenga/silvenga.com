665ae54844dc42fcf389382085c7c77b5e2507c5
Mark Lopez <m@silvenga.com>
2024-07-09 12:12:39 -0500

    Improved screen reader support for the footer.

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -3,13 +3,15 @@ export function Footer(): JSX.Element {
     return (
         <footer className="py-9 flex flex-col justify-center items-center mt-auto">
             <div className="text-center">
-                Copyright © {year}. Built with <span className="text-[#ad4d4d] dark:text-[#deabab]">&#x2764;&#xfe0e;</span> by Silvenga
+                Copyright © {year}. Built with <span className="sr-only">love</span><span className="text-[#ad4d4d] dark:text-[#deabab]" aria-hidden>&#x2764;&#xfe0e;</span> by Silvenga.
             </div>
             <div className="text-center">
                 Metrics gathered by Umami, privacy-focused-analytics. No cookies are saved.
             </div>
             <div className="text-center">
-                <a className="link link-hover" href="https://github.com/Silvenga/silvenga.com" rel="noreferrer noopener" target="_blank">[Source Code]</a>
+                <span aria-hidden>[</span>
+                <a className="link link-hover" href="https://github.com/Silvenga/silvenga.com" rel="noreferrer noopener" target="_blank">Source Code</a>
+                <span aria-hidden>]</span>
             </div>
         </footer>
     )
